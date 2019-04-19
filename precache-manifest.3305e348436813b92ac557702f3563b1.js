740b238f80721f14720f80052a8a4ffde3198676
Shenyu <lishenyu16@gmail.com>
2019-04-18 22:50:34 -0700

    Updates

---
+++ precache-manifest.3305e348436813b92ac557702f3563b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-react/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "99fdfe1fb05fdd8abaf2",
+    "url": "/burger-react/static/js/runtime~main.ebdee34b.js"
+  },
+  {
+    "revision": "18755b504834ef2ce8a4",
+    "url": "/burger-react/static/js/main.c7a1e3d0.chunk.js"
+  },
+  {
+    "revision": "33f2ba14a71b3c304c8c",
+    "url": "/burger-react/static/js/2.86429f0a.chunk.js"
+  },
+  {
+    "revision": "18755b504834ef2ce8a4",
+    "url": "/burger-react/static/css/main.5b1a3d9b.chunk.css"
+  },
+  {
+    "revision": "7278b681124dda8b11f5ce142f8af11d",
+    "url": "/burger-react/index.html"
+  }
+];
